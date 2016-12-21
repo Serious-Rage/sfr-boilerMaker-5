8de70b232976d57ddc97d8f388360cef8673842b
Brandon McAlpine <hughmann.spirit@gmail.com>
2016-12-21 04:56:50 -0800

    ide settings

--- js/rage.js
+++ js/rage.js
@@ -1,261 +1,232 @@
-
-
-
- /*
+/*
 
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
- 
+
  # code created by Brandon McAlpine (Serious Rage) SFR-Web development
  # licenced under mit non-commercial
  # contact Serious Rage    app-admin@seriousrageweb.com 
- 
+
  - - - - -- - -for commercial liscencing - - - - - - - - - - 
- 
+
  # visit SFR-Web development http://seriousrageweb.com
  # Serious Rage on gitHub - https://github.com/Serious-Rage
- 
+
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-Todo #1:
-build constructor for web Frames 
+ Todo #1:
+ build constructor for web Frames
 
 
-*/
+ */
 
 var SFR = function ($) {
 
-	var privateCounter = 0;
-	var URL_UI_inputs = (function ()
+    var privateCounter = 0;
+    var URL_UI_inputs = (function () {
 
-        {
-
-		(function
+        (function
             () {
-			var thing2 =
-            "<img src=\"";
-			var person2 =
-            "\" alt=\"badpic\">";
-			var thing =
+            var thing2 =
+                "<img src=\"";
+            var person2 =
+                "\" alt=\"badpic\">";
+            var thing =
 
-            "<iframe width=\"1060\" height=\"720\" src=\"";
+                "<iframe width=\"1060\" height=\"720\" src=\"";
 
-			var person =
+            var person =
 
-            "\" frameborder=\"1\" scrolling=\"no\" allowfullscreen></iframe>";
+                "\" frameborder=\"1\" scrolling=\"no\" allowfullscreen></iframe>";
 
-			var newText = document.
+            var newText = document.getElementById("myInput").value;
 
-            getElementById("myInput").value;
-
-			document.getElementById("stylesChanger")
+            document.getElementById("stylesChanger")
 
                 .onclick = function () {
 
-				newText = document.
+                newText = document.getElementById("myInput").value;
 
-                getElementById("myInput").value;
+                document.getElementById(
+                    "myInput2").value = thing + newText + person;
 
-				document.getElementById(
+            }
 
-        "myInput2").value = thing + newText + person;
+        })();
 
-			}
+        (function () {
 
-		})();
+                var thing2 = "<img src=\"";
+                var person2 = "\" alt=\"Brandon\">";
+                var thingYY =
 
-		(function ()
-            {
+                    "<iframe width=\"1100\" height=\"720\" src=\"";
 
-			var thing2 = "<img src=\"";
-            var person2 = "\" alt=\"Brandon\">";
-			var thingYY =
+                var personYY =
 
-            "<iframe width=\"1100\" height=\"720\" src=\"";
+                    "\" frameborder=\"1\" scrolling=\"yes\" allowfullscreen></iframe>";
 
-			var personYY =
+                var newText3 =
 
-            "\" frameborder=\"1\" scrolling=\"yes\" allowfullscreen></iframe>";
+                    document.getElementById("myInputYY").value;
 
-			var newText3 =
+                document.getElementById("stylesChanger3").onclick = function () {
 
-            document.getElementById("myInputYY").value;
+                    newText3 = document.getElementById("myInputAA").value;
 
-			document.
+                    document.getElementById("myInputBB").value = thing2 + newText3 + person2;
 
-                getElementById("stylesChanger3").onclick = function () {
+                }
 
-                newText3 = document.getElementById("myInputAA").value;
+            })();
 
-				document.
+        (function () {
+                var
+                    thing2 = "<img src=\"";
+                var person2 = "\" alt=\"badpic\">";
 
-        getElementById("myInputBB").
+                var thingYY = "<iframe width=\"1100\" height=\"720\" src=\"";
 
-        value = thing2 + newText3 + person2;
+                var personYY =
 
-			}
+                        "\" frameborder=\"1\" scrolling=\"yes\" allowfullscreen></iframe>"
 
-		}
-            )();
+                    ;
 
-		(function () {
-			var
-            thing2 = "<img src=\"";
-			var person2 = "\" alt=\"badpic\">";
+                var newText2 = document.getElementById("myInputYY").value;
 
-            var thingYY = "<iframe width=\"1100\" height=\"720\" src=\"";
+                document.getElementById("stylesChanger2").onclick =
 
-			var personYY =
+                    function () {
 
-            "\" frameborder=\"1\" scrolling=\"yes\" allowfullscreen></iframe>"
+                        newText2 = document.getElementById("myInputYY").value;
+                        document.getElementById(
+                            "myInputZZ").value = thingYY + newText2 + personYY;
 
-            ;
+                    }
+            })();
 
-			var newText2 = document.getElementById("myInputYY").value;
-
-			document.getElementById("stylesChanger2").onclick =
-
-                function () {
-
-				newText2 = document.getElementById("myInputYY").value;
-        document.getElementById(
+    });
 
 
-    "myInputZZ").value = thingYY + newText2 + personYY;
+    // MAKE IFRAMES ************
 
-			}
-		}
-    )();
-
-	});
-
-
-        // MAKE IFRAMES ************	
-
-	// private function	
-	var makeFram1 =
+    // private function
+    var makeFram1 =
         function (str) {
 
-        var Odiv = document.getElementsByClassName("outer");
-			Odiv = Odiv[0];
-		
-		    var NSdiv =
+            var Odiv = document.getElementsByClassName("outer");
+            Odiv = Odiv[0];
 
-        document.getElementsByClassName(
-        "noScrolling");
-	        NSdiv = NSdiv[0]
-        ;	
-		
-		    var x =
-        document.createElement("IFRAME");
-		x.
-        setAttribute('class', "inner");
-		x.
-        setAttribute('src', str);
-		x.setAttribute('scrolling', "no");
-		x.setAttribute('seamless'
+            var NSdiv =
 
-    ,
+                document.getElementsByClassName(
+                    "noScrolling");
+            NSdiv = NSdiv[0]
+            ;
+
+            var x =
+                document.createElement("IFRAME");
+            x.setAttribute('class', "inner");
+            x.setAttribute('src', str);
+            x.setAttribute('scrolling', "no");
+            x.setAttribute('seamless'
+
+                ,
 
 
-    "seamless");
-		x.setAttribute(
-        'allowfullscreen',
-     "allowfullscreen");
-		
-		NSdiv.appendChild(
-     x);	
-		
-	}
+                "seamless");
+            x.setAttribute(
+                'allowfullscreen',
+                "allowfullscreen");
+
+            NSdiv.appendChild(
+                x);
+
+        }
 
 
-     // Public function
-	function mkFrm1(str) {
-		makeFram1(str);
-	}
-
-
+    // Public function
+    function mkFrm1(str) {
+        makeFram1(str);
+    }
 
 
     /*
-	 <div class="outer">
-      <div class="noScrolling">
-        <iframe scrolling="no" seamless="seamless" class="inner" src="http://xhamster.com/xembed.php?video=1102053" frameborder="0" allowfullscreen></iframe>
-      </div>
-	  
-	  SFR-container
-	*/
+     <div class="outer">
+     <div class="noScrolling">
+     <iframe scrolling="no" seamless="seamless" class="inner" src="http://xhamster.com/xembed.php?video=1102053" frameborder="0" allowfullscreen></iframe>
+     </div>
+
+     SFR-container
+     */
 
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	// *********************************************************************************
+    // *********************************************************************************
     // ************************ OVERLAY NAV CONTROLS OPEN AND CLOSE ********************
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-	// private function	
-	function openNav() {
-		document.getElementById(
-    "mySidenav"
+    // private function
+    function openNav() {
+        document.getElementById(
+            "mySidenav"
+        ).style.width =
+            "26%";
+    }
 
-    ).style.width =
-    "26%";
-	}
+    function
+    closeNav() {
 
-	function
-        closeNav() {
+        document.getElementById(
+            "mySidenav").style.width = "0";
+    }
 
-    document.getElementById(
-    "mySidenav").style.width = "0";
-	}
+    // Public function
+    function opnNav() {
+        openNav();
+    }
 
-	// Public function		
-	function opnNav() {
-		openNav();
-	}
-
-	function
+    function
 
     clsNav() {
-		closeNav();
-	}
-	// ****************************************************************************************
+        closeNav();
+    }
 
-
-
-
+    // ****************************************************************************************
 
 
     function privateFunction() {
         privateCounter++;
-	}
+    }
 
     function publicFunction() {
         publicIncrement();
-	}
+    }
+
     function
 
 
-publicIncrement() {
-		privateFunction();
-	}
+    publicIncrement() {
+        privateFunction();
+    }
 
-	function publicGetCount() {
-		//return privateCounter++;
-	}
-	
-	
-	
+    function publicGetCount() {
+        //return privateCounter++;
+    }
+
+
 // ********************************* webVid vidKey IIFE wip************************************
     //<iframe width="510" height="400" src="http://xhamster.com/xembed.php?video=2492943" frameborder="0" scrolling="no" allowfullscreen></iframe>
-	// IIFE TEMPLATE
+    // IIFE TEMPLATE
 
-     // maybe keep records of host vars on static file to be iterated to form the switch /* ex: (one line as follows)
-	/*
-	
-	<iframe width=\"510\" height=\"400\" src=\"http://xhamster.com/xembed.php?video=|\" frameborder=\"0\" scrolling=\"no\" allowfullscreen></iframe>|
-	
-	*/
+    // maybe keep records of host vars on static file to be iterated to form the switch /* ex: (one line as follows)
+    /*
+
+     <iframe width=\"510\" height=\"400\" src=\"http://xhamster.com/xembed.php?video=|\" frameborder=\"0\" scrolling=\"no\" allowfullscreen></iframe>|
+
+     */
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // *********************************************************************************
@@ -263,41 +234,60 @@ publicIncrement() {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
+    function host(thatHost) {
 
-                function host(thatHost) {
-		
-		if (thatHost == "xh") {
-		return function(vidKey) {
-							  var lead =
+        if (thatHost == "xh") {
+            return function (vidKey) {
+                var lead =
 
-                "<iframe width=\"510\" height=\"400\" src=\"http://xhamster.com/xembed.php?video=";
-			
-							  var tail = "\" frameborder=\"0\" scrolling=\"no\" allowfullscreen></iframe>";
-            var strungOut = (lead + vidKey + tail);
-			                  
-							  }  
-		}
+                    "<iframe width=\"510\" height=\"400\" src=\"http://xhamster.com/xembed.php?video=";
 
-       return function(vidKey) {
-       console.
+                var tail = "\" frameborder=\"0\" scrolling=\"no\" allowfullscreen></iframe>";
+                var strungOut = (lead + vidKey + tail);
 
+            }
+        }
 
-    log(whattosay + ' there ' + name);
-   }
+        return function (vidKey) {
+            console.log(whattosay + ' there ' + name);
+        }
 
-}
-	// ****************************************************************************************
+    }
+
+    // ****************************************************************************************
 
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // *********************************************************************************
+    // ************** CREATE ELEMENTS ********************
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
+    var addrss = 'http://www.google.co.uk';
 
+
+// Private
+    function makeArticle(addrss) {
+
+        var element = document.createElement('a');
+
+        // Modify element properties
+        element.innerText = 'Google';
+        element.setAttribute('href', addrss);
+
+        // Add to body
+        document.body.appendChild(element);
+    }
+
+// Public
+    function ar_tickle(addrs) {
+        makeArticle(addrss);
+    }
 
 
     // Private Variable
     //var xham = host('xh');
-	//xham('vidKey');
-
+    //xham('vidKey');
 
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -306,41 +296,38 @@ publicIncrement() {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-
-
-        return {
-		start:
-        publicFunction,
-    increment: publicIncrement,
-		count: publicGetCount,
-		mkFrm1: mkFrm1,
-
- opnNav: opnNav,
-		clsNav: clsNav
-	};
+    return {
+        start: publicFunction,
+        increment: publicIncrement,
+        count: publicGetCount,
+        mkFrm1: mkFrm1,
+        opnNav: opnNav,
+        clsNav: clsNav,
+        ar_tickle: ar_tickle
+    };
 }($);
 
 
- // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- // *********************************************************************************
- // *********************************** NOTES ***************************************
- // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// *********************************************************************************
+// *********************************** NOTES ***************************************
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 /* - - - - - - - - - - - - - - - - -  example calls - - - - - - -  - - - - - - - - -
 
 
 
-//SFR.start();
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=3655311");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=5899460");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=2492943");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=2649123");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=5386211");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=5801688");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=3388174");
-SFR.mkFrm1("http://xhamster.com/xembed.php?video=4577434");
+ //SFR.start();
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=3655311");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=5899460");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=2492943");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=2649123");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=5386211");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=5801688");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=3388174");
+ SFR.mkFrm1("http://xhamster.com/xembed.php?video=4577434");
 
 
 
-*/
+ */
